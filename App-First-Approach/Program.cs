476d974bbc72ecde7a9f6474607504deeffb79e2
Beks_jf39@student.itstep.kz <Beks_jf39@student.itstep.kz>
2022-11-11 21:16:22 +0600

    Добавьте файлы проекта.

---
+++ App-First-Approach/Program.cs
@@ -0,0 +1,53 @@
+﻿using App_First_Approach.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace App_First_Approach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Scaffold-DbContext 'Data Source=223-10;Initial Catalog=ShopDb;Trusted_Connection=true;Encrypt=false' Microsoft.EntityFrameworkCore.SqlServer -ContextDir Data -OutputDir Models
+
+            using var dbContext = new ShopDbContext();
+            try
+            {
+                Console.WriteLine("Enter login: ");
+                var login = Console.ReadLine() ?? throw new InvalidOperationException("No Login");
+
+                //var user = dbContext.Users.Include(d => d.Baskets).First(f => f.Login == login);
+
+                var user = dbContext.Users.Include(d => d.Baskets).ThenInclude(d => d.BasketProducts).ThenInclude(p => p.Product).Where(w => w.Baskets.Any(a => a.BasketProducts.Any())).First(f => f.Login == login);
+                //foreach (var userBasket in user.Baskets)
+                //{
+                //    Console.WriteLine($"\t{userBasket.Id}, {userBasket.CreateDate}");
+                //    var product = dbContext.BasketProducts.Include(f => f.Product).FirstOrDefault(f => f.BasketId == userBasket.Id);
+                //    Console.WriteLine($"\t\t{product.Product.Name})");
+
+                //    if (product != null) 
+                //    {
+                //        Console.WriteLine($"Product =  ${userBasket.User.Name} gas"); 
+                //    }
+                //}
+                Console.Clear();
+                Console.WriteLine($"User with loin - {user.Login} has following baskets: ");
+
+                foreach (var userBasket in user.Baskets)
+                {
+                    foreach (var userBasketProduct in userBasket.BasketProducts)
+                    {
+                        Console.WriteLine($"User {userBasket.User.Name} has - {userBasketProduct.Product.Name}"); 
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e); ;
+                throw;
+            }
+        }
+    }
+}
+
+//SELECT    нельзя использовать EntityFrameWork
